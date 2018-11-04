5e58f281af0c659ba1e1de75c5fbf0ab47a2786f
zetarop <arkupa199888@gmail.com>
2018-11-04 23:02:08 +0530

    Create minimum_coin_to_sum_n.cpp

---
+++ minimum_coin_to_sum_n.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ve vector
+#define pb push_back
+#define ll long long
+#define inf 1000000007
+
+using namespace std;
+
+int minimumCoin(ve<int> v, int sum)
+{
+	int dp[sum+1];
+	fill(dp, dp+sum+1, inf);
+	dp[0]=0;
+	for(int i=1;i<=sum;i++)
+		for(auto a:v)
+			if(i >= a && dp[i-a] != inf)
+				dp[i] = min(dp[i], 1+dp[i-a]);
+	for(int i=0;i<=sum;i++)
+		cout<<dp[i]<<" ";
+	cout<<endl;
+
+	return dp[sum] == inf ? -1 : dp[sum];
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	ve<int> v(n);
+	for(auto &x:v)
+		cin>>x;
+
+	int sum;
+	cin>>sum;
+
+	cout<<minimumCoin(v, sum)<<endl;
+	return 0;
+}
