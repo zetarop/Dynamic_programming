f0dcbbf8e1fc1233acaa6c52490fc1bc1ab43446
zetarop <arkupa199888@gmail.com>
2018-11-10 12:10:27 +0530

    Create subset_sum_problem.cpp

---
+++ subset_sum_problem.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ve vector
+#define pb push_back
+#define ll long long
+#define inf 1000000007
+
+using namespace std;
+
+
+int solve()
+{
+	int n;
+	int sum;
+	cin>>n;
+	ve<int> v(n);
+	for(auto &x:v)
+		cin>>x;
+	int dp[sum+1];
+	fill(dp, dp+sum+1,0);
+	dp[0]=1;
+	for(auto x:v)
+		for(int i=sum;i>=x;i--)
+			dp[i] = dp[i] || dp[i-x];
+	return dp[sum];
+}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+		cout<<solve()<<endl;
+}
